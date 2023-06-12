fb191d9075e3073b4a4634db442067300a21f1e9
Omkar Sutar <ipad.omkarsutar@gmail.com>
2023-06-12 12:15:46 +0530

    Hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React from nitesh he is Badsaha
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+function App()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>
+      Omkar <br></br>
+      Nitesh <br></br>
+      Amit
+    </h1>
   );
+  
 }
+
+
+
+
+
+
+
+
+
+
 
 export default App;
